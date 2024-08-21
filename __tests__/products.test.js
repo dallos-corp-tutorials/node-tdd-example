4fbca147c85ef2c86c2c56ee9833bda202b8b170
cristiandallos <cristiandaviddallosbustos@gmail.com>
2024-08-20 21:22:01 -0500

    :sparkles: Refactor code: Add builder to test product

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -1,24 +1,20 @@
 const request = require('supertest')
 const { app } = require('../server')
-
+const { Builder } = require('../builders/product-builder.js')
 
 describe('POST /products', () => {
     test('should store a new product', async () => {
-        const response = await request(app)
+      
+      const product = Builder.product()  
+      const response = await request(app)
           .post('/products')
-          .send({
-            name: 'my product',
-            description: 'this is a test',
-            price: '100',
-          })
+          .send(product)
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(201)
       
         expect(response.body).toEqual({
-          name: 'my product',
-          description: 'this is a test',
-          price: '100',
+          ... product,
           _id: 'abc',
         })
       })
