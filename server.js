6c59ad261b8903b4f56c7ea7570e321ab375e196
cristiandallos <cristiandaviddallosbustos@gmail.com>
2024-08-20 20:41:10 -0500

    :sparkles: Add endpoint to fix first test

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.post('/products', (req, res) => {
+    res.status(201).json()
+})
+
 if (require.main === module) {
     app.listen(() => console.log(`listening on port ${port}`))
 }
