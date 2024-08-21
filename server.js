f9a907af23fde5b909b6516e71e701f43ec6bee1
cristiandallos <cristiandaviddallosbustos@gmail.com>
2024-08-20 21:15:04 -0500

    :sparkles: Refactor code

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-const express = require('express')
-const app = express()
+const { app } = require('./app')
+
 const port = 8080
 
-app.use(express.json())
-app.post('/products', (req, res) => {
-    const { name, description, price } = req.body
-    const _id = 'abc'
-    res.status(201).json({
-        name,
-        description,
-        price,
-        _id,
-    })
-})
-
 if (require.main === module) {
-    app.listen(() => console.log(`listening on port ${port}`))
+  app.listen(() => console.log(`listening on port ${port}`))
 }
 
 module.exports.app = app
