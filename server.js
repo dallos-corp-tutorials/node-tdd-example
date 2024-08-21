10685bf657cffd8eb03410e4c081595ffc88b018
cristiandallos <cristiandaviddallosbustos@gmail.com>
2024-08-20 21:01:04 -0500

    :sparkles: Add return body to endpoint to fix test

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.json())
 app.post('/products', (req, res) => {
-    res.status(201).json()
+    const { name, description, price } = req.body
+    const _id = 'abc'
+    res.status(201).json({
+        name,
+        description,
+        price,
+        _id,
+    })
 })
 
 if (require.main === module) {
